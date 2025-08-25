2d3a21679d52234a357a0ae74644839090251857
John Duavis <paulduavis09@gmail.com>
2025-08-25 23:33:19 +0800

    Add under construction message to Experience page and added certification in NavBar.razor

--- PortV2/obj/Debug/net9.0/PortV2.AssemblyInfo.cs
+++ PortV2/obj/Debug/net9.0/PortV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88da61de6675de527da479759eab229df140853")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a92c2b469e572ef5744049bfe738e638b25119")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
