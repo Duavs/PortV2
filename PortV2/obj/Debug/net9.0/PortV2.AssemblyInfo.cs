f19dbfd6912cc73ee53be5e1b47c01a02d5b7b6d
John Duavis <paulduavis09@gmail.com>
2025-09-13 23:29:18 +0800

    refactor About page: remove unnecessary icon and clean up iframe structure

--- PortV2/obj/Debug/net9.0/PortV2.AssemblyInfo.cs
+++ PortV2/obj/Debug/net9.0/PortV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4283e95bfbfc1d0b6264f5821c70dfe9f3ddbae5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0dde9281720dd3fd110ef0cdbbc46a671156b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
